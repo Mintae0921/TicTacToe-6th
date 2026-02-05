4d84b367da3f28330a1bebaa657823e5e5628f3c
Mintae0921 <pantack0921@gmail.com>
2026-02-05 17:14:25 +0900

    오후시간커밋
    옵션 UI

--- Assets/02. Scripts/MainPanelController.cs
+++ Assets/02. Scripts/MainPanelController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class MainPanelController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
